06b86e7be0fe7c9155880fa4bc63a1941b2a6378
atulsanagar <atul.sanagar@gmail.com>
2018-02-04 00:00:35 +0530

    removed class files from /src/main/java

--- src/main/java/dbmi/hms/harvard/edu/testplans/results/SearchBySubjectResults1.java
+++
@@ -1,5 +0,0 @@
-package dbmi.hms.harvard.edu.testplans.results;
-
-public class SearchBySubjectResults1 extends Results{
-
-}
--- src/main/java/dbmi/hms/harvard/edu/testplans/results/SuccessTypes1.java
+++
@@ -1,5 +0,0 @@
-package dbmi.hms.harvard.edu.testplans.results;
-
-public enum SuccessTypes1 {
-	patientcountsubset1, searchbysubject
-}
