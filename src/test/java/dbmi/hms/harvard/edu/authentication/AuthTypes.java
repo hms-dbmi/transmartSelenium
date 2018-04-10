3ce4f87355ccb166b26f84014596f3564afb9054
atulsanagar <atul.sanagar@gmail.com>
2018-04-10 23:21:21 +0530

    Added LoginMethod for Login to GRDR using Google Account

--- src/test/java/dbmi/hms/harvard/edu/authentication/AuthTypes.java
+++ src/test/java/dbmi/hms/harvard/edu/authentication/AuthTypes.java
@@ -5,62 +5,83 @@ import java.util.Map;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.BeforeTest;
-
-import dbmi.hms.harvard.edu.testplans.Testplan;
 
 public class AuthTypes {
-	
-	
-	
-	Map<String, String> authTypes = new HashMap<String,String>()
-			{{
-				put("HMS", "Harvard Medical School");
-				
-			}};
 
-			
-	public void doAuth(WebDriver driver, Map testPlan)	{
-		switch(testPlan.get("authmethod").toString()){
-			case "HMS": doHMSAuth(driver, testPlan); 
-			case "PUB": dopublicauth(driver,testPlan);
-						break;
+	Map<String, String> authTypes = new HashMap<String, String>() {
+		{
+			put("HMS", "Harvard Medical School");
+
+		}
+	};
+
+	public void doAuth(WebDriver driver, Map testPlan) {
+		switch (testPlan.get("authmethod").toString()) {
+		case "HMS":
+			doHMSAuth(driver, testPlan);
+		case "PUB":
+			dopublicauth(driver, testPlan);
+			break;
+		case "GOOGLELOGIN":
+			doGoogleLogin(driver, testPlan);
+			break;
+		
 		}
 	}
-			
-	public void doHMSAuth(WebDriver driver, Map testPlan){
-	    try {
-	    	String usernamebox = ".//*[@id='ctl00_ContentPlaceHolder1_UsernameTextBox']";
-	    	String passwordbox = "//*[@id='ctl00_ContentPlaceHolder1_PasswordTextBox']";
-	    	String submitbutton = "//*[@id='ctl00_ContentPlaceHolder1_SubmitButton']";
 
+	private void doGoogleLogin(WebDriver driver, Map testPlan) {
+
+		try {
+			String usernamebox = ".//*[@id='ctl00_ContentPlaceHolder1_UsernameTextBox']";
+			String passwordbox = "//*[@id='ctl00_ContentPlaceHolder1_PasswordTextBox']";
+			String submitbutton = "//*[@id='ctl00_ContentPlaceHolder1_SubmitButton']";
 
 			driver.findElement(By.xpath(usernamebox)).sendKeys(testPlan.get("username").toString());
 			driver.findElement(By.xpath(passwordbox)).sendKeys(testPlan.get("password").toString());
 
-			
 		} catch (SecurityException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	    
-	   // driver.findElement(By.xpath("//*[@id='ctl00_ContentPlaceHolder1_PasswordTextBox']")).sendKeys(PASSWORD);
+
+		
+		
+
+		
+		
 	}
-	public void dopublicauth(WebDriver driver, Map testPlan){
-	    try {
-	    	
-	    	String publicLogin = ".//*[@id='public']";
-	    	driver.findElement(By.xpath(publicLogin)).click();
-	    	
-			
+
+	public void doHMSAuth(WebDriver driver, Map testPlan) {
+		try {
+			String usernamebox = ".//*[@id='ctl00_ContentPlaceHolder1_UsernameTextBox']";
+			String passwordbox = "//*[@id='ctl00_ContentPlaceHolder1_PasswordTextBox']";
+			String submitbutton = "//*[@id='ctl00_ContentPlaceHolder1_SubmitButton']";
+
+			driver.findElement(By.xpath(usernamebox)).sendKeys(testPlan.get("username").toString());
+			driver.findElement(By.xpath(passwordbox)).sendKeys(testPlan.get("password").toString());
+
 		} catch (SecurityException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	    
-	   // driver.findElement(By.xpath("//*[@id='ctl00_ContentPlaceHolder1_PasswordTextBox']")).sendKeys(PASSWORD);
+
+		// driver.findElement(By.xpath("//*[@id='ctl00_ContentPlaceHolder1_PasswordTextBox']")).sendKeys(PASSWORD);
 	}
-			
+
+	public void dopublicauth(WebDriver driver, Map testPlan) {
+		try {
+
+			String publicLogin = ".//*[@id='public']";
+			driver.findElement(By.xpath(publicLogin)).click();
+
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// driver.findElement(By.xpath("//*[@id='ctl00_ContentPlaceHolder1_PasswordTextBox']")).sendKeys(PASSWORD);
+	}
+
 	public Map<String, String> getAuthTypes() {
 		return authTypes;
 	}
@@ -68,9 +89,5 @@ public class AuthTypes {
 	public void setAuthTypes(Map<String, String> authTypes) {
 		this.authTypes = authTypes;
 	}
-			
-			
+
 }
-
-
-
